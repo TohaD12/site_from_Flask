e23027a5136c43150f2710c3d96fd58bce649ef6
toha <smssobaka@gmail.com>
2021-02-10 00:34:28 +0300

    updayte
    Добавлена страница создания контекста добавлены формы функция обработки и сохранения в базу пост запросов

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///content.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
@@ -12,7 +13,7 @@ class Articles(db.Model):
     name = db.Column(db.String(50), nullable=False)
     intro = db.Column(db.String(200), nullable=False)
     text = db.Column(db.Text, nullable=False)
-    text = db.Column(db.DateTime, default=datetime.utcnow)
+    release_date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Articles %r>' % self.id
@@ -26,6 +27,25 @@ def index():
 @app.route('/about_me')
 def about():
     return render_template("about.html")
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        article = Articles(name=title, intro=intro, text=text)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+
+            return print("Error")
+
+    else:
+        return render_template("create.html")
 
 
 if __name__ == "__main__":
